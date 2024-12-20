09a36bcfef712f6db5b14ec7a68d64c250b8f34c
Abel_personal <abel.aguilarc8@gmail.com>
2024-12-20 18:25:34 +0000

    Back at it again

--- src/main.c
+++ src/main.c
@@ -5,9 +5,9 @@ int main(void)
 {   
     heap_t *heap = hp_alloc_heap(10);
     hp_insert(heap, 10);
-    hp_insert(heap, 20);
     hp_insert(heap, 30);
+    hp_insert(heap, 20);
+    hp_print_heap(heap);
     hp_free_deep_heap(heap);
-    printf("Hello, World!\n");
     return 0;
 }
