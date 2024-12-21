d59b32440235bd10b22f70de022cf5665dbb46c6
Abel_personal <abel.aguilarc8@gmail.com>
2024-12-21 01:41:45 +0000

    Naive implementation of linkedlist

--- src/main.c
+++ src/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
-#include "../include/heap.h"
+#include "../include/linkedlist.h"
 
 int main(void) 
 {   
-    heap_t *heap = hp_alloc_heap(10);
-    hp_insert(heap, 10);
-    hp_insert(heap, 30);
-    hp_insert(heap, 20);
-    hp_print_heap(heap);
-    hp_free_deep_heap(heap);
+    head = ll_create_node(2);
+
+    ll_insert_node_head(ll_create_node(1));
+    ll_insert_node_tail(ll_create_node(3));
+    ll_insert_node_at(ll_create_node(4), 1);
+
+    // ll_delete_node_head();
+    // ll_delete_node_tail();
+    // ll_delete_node_head();
+    // ll_delete_node_at(0);
+
+    printf("Length: %zu\n", ll_length());
+
+    ll_print_list_from(head);
+    ll_search_full_list(4, ll_print_node);
+
+    ll_freeDeep_node(head);
+
     return 0;
 }
