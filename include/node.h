09dd06ef8e8d6710a7acc5c06fbd473afd429380
Abel_personal <abel.aguilarc8@gmail.com>
2025-01-02 15:02:35 +0000

    Implemented doubly linked list node

--- include/node.h
+++ include/node.h
@@ -18,6 +18,18 @@ typedef struct single_node sl_node_t;
 sl_node_t* n_create_node(int data);
 void n_free_node(sl_node_t* node);
 
-void n_print_node(node_t* node);
+void n_print_node(sl_node_t* node);
+
+struct double_node {
+    int data;
+    struct double_node *next;
+    struct double_node *prev;
+};
+typedef struct double_node dl_node_t;
+
+dl_node_t* n_create_dnode(int data);
+void n_free_dnode(dl_node_t* node);
+
+void n_print_dnode(dl_node_t* node);
 
 #endif // NODE_H
--- src/node.c
+++ src/node.c
@@ -17,11 +17,41 @@ void n_print_node(sl_node_t* node){
     printf("Node : {ref = %p; data = %d, next = %p}\n", node, node->data, node->next);
 }
 
-void n_free_node(node_t* node){
+void n_free_node(sl_node_t* node){
     if(node_is_null(node)){
         return;
     }
 
     node->data = 0;
+    node->next = NULL;
+    free(node);
+}
+
+dl_node_t* n_create_dnode(int data){
+    dl_node_t* node = (dl_node_t*)malloc(sizeof(dl_node_t));
+    assert(node_is_not_null(node) && "Memory allocation failed");
+
+    node->data = data;
+    node->next = NULL;
+    node->prev = NULL;
+    return node;
+}
+
+void n_print_dnode(dl_node_t* node){
+    if(node_is_null(node)){
+        printf("Node is null\n");
+        return;
+    }
+    printf("Node : {ref = %p; data = %d, next = %p, prev = %p}\n", node, node->data, node->next, node->prev);
+}
+
+void n_free_dnode(dl_node_t* node){
+    if(node_is_null(node)){
+        return;
+    }
+
+    node->data = 0;
+    node->next = NULL;
+    node->prev = NULL;
     free(node);
 }
