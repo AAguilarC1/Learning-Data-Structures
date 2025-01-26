ccfa3a13bb692ac4f715e85c6a240736879128c8
Abel_personal <abel.aguilarc8@gmail.com>
2025-01-26 17:53:01 +0000

    Idk why this is asking for changes

---
+++ deprecated/test_min_heap.h
@@ -0,0 +1,17 @@
+#ifndef TEST_MIN_HEAP_H
+#define TEST_MIN_HEAP_H
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "heap.h"
+
+void testAllocateHeap();
+void testAllocateHeapFromArray();
+void testInsertIntoHeapArray();
+void testInsert();
+
+int test_min_heap_suite(CU_pSuite min_heap_suite);
+
+#endif // TEST_MIN_HEAP_H
