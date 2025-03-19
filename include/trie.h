562317358b5cbdbc3dc9a6490e2a0c358e065c52
Abel_personal <abel.aguilarc8@gmail.com>
2025-03-19 20:43:06 +0000

    Initial commit: New feature trie data structure
    The trie, also known as the prefix tree, is a data structure used to
    store strings for quick retrieval

---
+++ include/trie.h
@@ -0,0 +1,10 @@
+#ifndef TRIE_H
+#define TRIE_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "arena_allocator.h"
+
+void noop();
+
+#endif // TRIE_H
---
+++ src/trie.c
@@ -0,0 +1,5 @@
+#include "trie.h"
+
+void noop() {
+  return;
+}
