a3f8f075d4ef9d02ae847db062e1631b1a09f95e
Abel_personal <abel.aguilarc8@gmail.com>
2025-03-20 20:29:41 +0000

    Arena allocator for trie

--- include/trie.h
+++ include/trie.h
@@ -5,6 +5,10 @@
 #include <string.h>
 #include "arena_allocator.h"
 
-void noop();
+#define ARENA_SIZE KB(10)
+#define ARENA_PAGES 10
+
+void init_trie_arena(void);
+void destroy_trie_arena(void);
 
 #endif // TRIE_H
--- src/trie.c
+++ src/trie.c
@@ -1,5 +1,14 @@
 #include "trie.h"
+#include "arena_allocator.h"
+static const arena_t* trie_arena;
 
-void noop() {
-  return;
+void init_trie_arena(void){
+  trie_arena  = malloc(sizeof(arena_t)); 
+  arena_t temp = arena_init(ARENA_SIZE, ARENA_PAGES);
+  memcpy(trie_arena, &temp, sizeof(arena_t));
 }
+
+void destroy_trie_arena(void){
+  arena_destroy(trie_arena);
+}
+
