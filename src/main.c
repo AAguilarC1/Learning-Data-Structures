5f2e88ffed2f83780e23db6583bc2aedba7440de
Abel_personal <abel.aguilarc8@gmail.com>
2025-03-20 20:30:08 +0000

    Change for trie testing in main

--- src/main.c
+++ src/main.c
@@ -1,67 +1,8 @@
-#include "arena_allocator.h"
+#include "trie.h"
 
-struct TestStruct{
-  int a;
-  int b;
-  int c;
-};
-typedef struct TestStruct test_struct;
-
-int main(void) 
-{  
-  arena_t arena = arena_init(KB(1), 3);
-  printf("Before allocation\n");
-  printf("Arena region capacity: %lu\n", arena.pool->capacity);
-  printf("Arena total size: %lu\n", arena.total_size);
-  printf("Arena num regions: %u\n", arena.num_regions_available);
-  printf("Max total size: %lu\n", arena.total_capacity);
-
-  size_t ptr3_len = (256)/sizeof(int32_t);
-  size_t ptr4_len = (256)/sizeof(double);
-  int32_t* ptr3 = (int32_t*) arena_alloc(&arena, ptr3_len * sizeof(int32_t));
-  double* ptr4 = (double*) arena_alloc_align(&arena, ptr4_len * sizeof(double), _Alignof(double));
-  char* ptr5 = (char*) arena_alloc(&arena, 256 * sizeof(char));
-
-  printf("After allocation\n");
-  printf("Arena total size: %lu\n", arena.total_size);
-  printf("Arena num regions available: %u\n", arena.num_regions_available);
-
-  if(ptr5 != NULL){ 
-    memcpy(ptr5, "Hello, World!", 14);
-    printf("ptr5: %s\n", ptr5);
-  }
-  
-  test_struct* ptr6 = (test_struct*) arena_alloc(&arena, 3*  sizeof(test_struct));
-  if(ptr6 != NULL){
-    ptr6->a = 10;
-    ptr6->b = 20;
-    ptr6->c = 30;
-
-    printf("ptr6->a: %i\n", ptr6->a);
-    printf("ptr6->b: %i\n", ptr6->b);
-    printf("ptr6->c: %i\n", ptr6->c);
-  }
-
-  if(ptr4 != NULL){
-    for(size_t i = 0; i < ptr4_len; i++){
-      ptr4[i] = rand() / (float) RAND_MAX; 
-    }
-
-    for(size_t i = 0; i < ptr4_len; i++){
-      printf("ptr4[%lu]: %f\n", i, ptr4[i]);
-    }
-  }
-
-  if(ptr3 != NULL){
-    for(size_t i = 0; i < ptr3_len; i++){
-      ptr3[i] = (uint16_t) i;
-    }
-
-    for(size_t i = 0; i < ptr3_len; i++){
-      printf("ptr3[%lu]: %i\n", i, ptr3[i]);
-    }
-  }
-  arena_destroy(&arena);
+int main(void) {  
+  init_trie_arena();
+  destroy_trie_arena();
 
   return 0;
 
