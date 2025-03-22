fcf15362066220c9b5a474dd26e6e8432983cd78
Abel_personal <abel.aguilarc8@gmail.com>
2025-03-22 00:10:22 +0000

    Testing the trie functions in Main

--- src/main.c
+++ src/main.c
@@ -1,9 +1,25 @@
 #include "trie.h"
 
 int main(void) {  
-  init_trie_arena();
-  destroy_trie_arena();
+  trie_t trie = trie_create();
+  trie_insert(&trie, "Hello");
+  trie_insert(&trie, "world");
+
+  trie_insert(&trie, "cat");
+  trie_insert(&trie, "dog");
+  trie_insert(&trie, "apple");
+  trie_insert(&trie, "car");
+
+  printf("hello: %d\n", trie_search(&trie, "heLlo"));
+  printf("world: %d\n", trie_search(&trie, "world"));
+  printf("cat: %d\n", trie_search(&trie, "cat"));
+  printf("dog: %d\n", trie_search(&trie, "dog"));
+  printf("apple: %d\n", trie_search(&trie, "apple"));
+  printf("car: %d\n", trie_search(&trie, "car"));
+
+  printf("hello world: %d\n", trie_search(&trie, "hello WorlD"));
+
+  trie_destroy(&trie);
 
   return 0;
-
 }
